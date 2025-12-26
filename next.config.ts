d335ba51e36a3f8e8bf8c764e97508b75b80bbca
pyae782003-blip <pyae782003@gmail.com>
2025-12-27 04:12:44 +0700

    Fix: Force include SQLite db in build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['prisma', '@prisma/client'],
+    outputFileTracingIncludes: {
+      '/': ['./prisma/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
